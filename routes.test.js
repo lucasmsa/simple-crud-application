7ee16ae73d1e36c41bfb2de0570d78dbb44549ff
Lucas Moreira <lucasmsea@outlook.com>
2020-05-28 20:05:39 -0300

    Jest route testing completed

--- routes.test.js
+++ routes.test.js
@@ -2,37 +2,71 @@ const { server, db } = require('./index.js')
 const request = require('supertest')
 const sqlite3 = require('sqlite3')
 
+insert_sql = `INSERT INTO Product (id_categoria, descricao) VALUES (?, ?)`
+new_product = [2, 'Planeta dos macacos']
+
+// At first a new product is inserted in the database so that it can be deleted 
+// afterwards to test the delete route
+db.run(insert_sql, new_product, err => {
+    if(err) {
+        return console.error(err.message)
+    } else {
+        console.log('Successfully inserted element')
+    }
+})
+
+select_sql = `SELECT * FROM Product ORDER BY id DESC LIMIT 1` 
+
+// Selecting the element that was just added and storing its id in a
+// global variable so that it can be accessed by the insert, update and delete routes
+db.all(select_sql, [], (err, row) => {
+    if(err) {
+        return console.error(err.message)
+    } else {
+        global.globalID = row[0].id
+    }
+})
+
+
 describe('Starting route testing', () => {
 
-    //descrição do caso de testes
     test('Displaying all prdoucts route', async () => {
-        //qual a rota que ele deve acessar e qual requisição deve fazer
         const response = await request(server).get('/products');
-        //qual o status esperado 
         expect(response.status).toEqual(200);
 
         expect(response.type).toEqual('application/json')
      });
 
      test('Displaying a specific prdouct', async () => {
-        const response = await request(server).get('/products/1');
+        const response = await request(server).get('/products/'+globalID);
 
         expect(response.status).toEqual(200);
         expect(response.type).toEqual('application/json')
      });
 
     test('Prdoucts insertion route', async () => {
-        //qual a rota que ele deve acessar e qual requisição deve fazer
         const response = await request(server).get('/insertProduct');
-        //qual o status esperado 
         expect(response.status).toEqual(200);
      });
 
      test('Prdoucts update route', async () => {
-        //qual a rota que ele deve acessar e qual requisição deve fazer
-        const response = await request(server).get('/updateProduct/1');
-        //qual o status esperado 
+        const response = await request(server).get('/updateProduct/'+globalID);
         expect(response.status).toEqual(200);
      });
-})
 
+     test('Deleting product from the database', async () => {
+        const response = await request(server).delete('/deleteProduct/'+globalID);
+        expect(response.status).toEqual(200);
+        expect(response.text).toEqual('Deleted product with id: ' + globalID)
+     });
+}); afterAll(async () => {
+        try {
+            console.log('All tests finished!')
+            await db.close();
+            await server.close();
+
+        } catch (error) {
+        console.log('Something is wrong ' + error);
+        throw error;
+    }
+})
